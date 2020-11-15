1339c367b33e76a71fab11f98cbb6e1258dcc9f2
chad-lashock <42807804+chad-lashock@users.noreply.github.com>
2020-11-14 19:27:40 -0600

    Add files via upload

--- capstone.py
+++ capstone.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import calculate_sma_profit
+from functions import calculate_wma_profit
+from functions import calculate_ema_profit
+from functions import calculate_rsi
+from functions import calculate_rsi_profit_50
 
+#Load and organize data
+
+#---CAT---
 cat = pd.read_csv("./data/CAT.csv")
 cat['Date'] = pd.to_datetime(cat['Date'])
 cat['Price'] = cat['Close']
@@ -12,6 +20,34 @@ cat['Date'] = cat['Date'].dt.strftime('%m/%d/%Y')
 cat['sma200'] = cat['Price'].rolling(200).mean()
 cat['sma50'] = cat['Price'].rolling(50).mean()
 
+#----MMM----
+mmm = pd.read_csv("./data/MMM.csv")
+#mmm.index = pd.to_datetime(mmm.Date)
+mmm['Price'] = mmm['Close']
+mmm = mmm.drop(['Open','High','Low','Close','Adj Close'], axis=1)
+
+mmm['sma200'] = mmm['Price'].rolling(200).mean()
+mmm['sma50'] = mmm['Price'].rolling(50).mean()
+
+weights200 = np.arange(1,201)
+weights50 = np.arange(1,51)
+mmm['wma200'] = mmm['Price'].rolling(200).apply(lambda x: np.dot(x,weights200)/weights200.sum())
+mmm['wma50'] = mmm['Price'].rolling(50).apply(lambda x : np.dot(x, weights50)/weights50.sum())
+
+#----VZ----
+vz = pd.read_csv("./data/VZ.csv")
+#vz.index = pd.to_datetime(vz.Date)
+vz['Price'] = vz['Close']
+vz = vz.drop(['Open','High','Low','Close','Adj Close'], axis=1)
+
+vz['ema50'] = vz['Price'].ewm(span=50, min_periods=50).mean()
+vz['ema200'] = vz['Price'].ewm(span=200, min_periods=200).mean()
+
+
+# Plots to show the stock prices and how some of the strategies work
+
+
+#----CAT----
 plt.figure(figsize = (12, 6))
 plt.plot(cat['Price'], label="Price", color="red")
 plt.legend()
@@ -25,73 +61,7 @@ plt.legend()
 plt.grid()
 plt.show()
 
-# function to calculate simple moving average profit
-def calculate_sma_profit(long_period, short_period, df):
-    long_ma = list(df['Price'].rolling(long_period).mean())
-    short_ma = list(df['Price'].rolling(short_period).mean())
-    results = pd.DataFrame(columns = ['Date','Action', 'Price', 'Bank', 'Shares'])
-
-    
-    bank = 1000
-    shares = 0
-    
-    if short_ma[long_period-1] < long_ma[long_period-1]:
-        short_ma_under = True
-    else:
-        short_ma_under = False
-    
-    for x in range(long_period, len(long_ma)):
-        if short_ma_under == True and short_ma[x] > long_ma[x]:
-            # buy
-            short_ma_under = False
-            if bank > 0:
-                shares = bank / df['Price'].loc[x]
-                bank = 0
-                results = results.append({'Date': df['Date'].loc[x], 'Action': 'Buy', 'Price': df['Price'].loc[x], 'Bank': bank, 'Shares': shares}, 
-                                         ignore_index=True)
-                
-                
-        elif short_ma_under == False and short_ma[x] < long_ma[x]:
-            # sell
-            short_ma_under = True
-            if bank == 0:
-                bank = shares * df['Price'].loc[x]
-                shares = 0
-                results = results.append({'Date':df['Date'].loc[x], 'Action': 'Sell', 'Price': df['Price'].loc[x], 'Bank': bank, 'Shares': shares}, 
-                                         ignore_index=True)
-    
-    
-    if bank == 0:
-        bank = shares * df['Price'].loc[x]
-        
-    return bank, results
-    
-            
-#Calculate sma profit for standard 200-day and 50-day
-calculate_sma_profit(200, 50, cat)
-
-sma_results = pd.DataFrame(columns=["short-period", "long-period", "bank"])
-
-for x in range(10,201,5):
-    for y in range(x+5,201,5):
-        bank, result = calculate_sma_profit(y, x, cat)
-        sma_results = sma_results.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
-    
-
-
-
-mmm = pd.read_csv("./data/MMM.csv")
-mmm.index = pd.to_datetime(mmm.Date)
-mmm['Price'] = mmm['Close']
-mmm = mmm.drop(['Open','High','Low','Close','Adj Close'], axis=1)
-
-mmm['sma200'] = mmm['Price'].rolling(200).mean()
-mmm['sma50'] = mmm['Price'].rolling(50).mean()
-
-weights200 = np.arange(1,201)
-weights50 = np.arange(1,51)
-mmm['wma200'] = mmm['Price'].rolling(200).apply(lambda x: np.dot(x,weights200)/weights200.sum())
-mmm['wma50'] = mmm['Price'].rolling(50).apply(lambda x : np.dot(x, weights50)/weights50.sum())
+#----MMM----
 
 plt.figure(figsize = (12,8))
 plt.plot(mmm['wma50'], label="50-day WMA", color="grey")
@@ -103,14 +73,8 @@ plt.xlabel("Date")
 plt.ylabel("Price ($)")
 plt.show()
 
-vz = pd.read_csv("./data/VZ.csv")
-vz.index = pd.to_datetime(vz.Date)
-vz['Price'] = vz['Close']
-vz = vz.drop(['Open','High','Low','Close','Adj Close'], axis=1)
 
-vz['ema50'] = vz['Price'].ewm(span=50, min_periods=50).mean()
-vz['ema200'] = vz['Price'].ewm(span=200, min_periods=200).mean()
-
+#----VZ----
 plt.figure(figsize = (12,8))
 plt.plot(vz['ema50'], label='50-day EMA',color='grey')
 plt.plot(vz['ema200'],label='200-day EMA',color='red')
@@ -122,13 +86,120 @@ plt.xlabel("Date")
 plt.show()
 
 
-#Relative strength
+            
+#Calculate sma profit for standard 200-day and 50-day
+calculate_sma_profit(200, 50, cat)
 
-#Input is array of price changes
-def calculate_rsi(x):
-    sum_of_gains = sum([a for a in x if a > 0])
-    sum_of_changes = sum([abs(a) for a in x])
-    return (sum_of_gains/sum_of_changes)*100
+
+#The following calculates the results of each CAT, MMM, and VZ for each of the 
+# short-term and long-term value pairs using the simple moving average crossover
+# strategy. The results are saved to a data frame.
+
+#----CAT----
+sma_results_cat = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_sma_profit(y, x, cat)
+        sma_results_cat = sma_results_cat.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+sma_results_cat = sma_results_cat.sort_values(by="bank", ascending=False)
+
+#----MMM----
+sma_results_mmm = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_sma_profit(y, x, mmm)
+        sma_results_mmm = sma_results_mmm.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+sma_results_mmm = sma_results_mmm.sort_values(by="bank", ascending=False)
+
+
+#----VZ----
+
+sma_results_vz = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_sma_profit(y, x, vz)
+        sma_results_vz = sma_results_vz.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+sma_results_vz = sma_results_vz.sort_values(by="bank", ascending=False)
+
+#The following calculates the results of each CAT, MMM, and VZ for each of the 
+# short-term and long-term value pairs using the weighted moving average crossover
+# strategy. The results are saved to a data frame.
+
+#----CAT----
+wma_results_cat = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_wma_profit(y, x, cat)
+        wma_results_cat = wma_results_cat.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+wma_results_cat = wma_results_cat.sort_values(by="bank", ascending=False)
+
+#----MMM----
+wma_results_mmm = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_wma_profit(y, x, mmm)
+        wma_results_mmm = wma_results_mmm.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+wma_results_mmm = wma_results_mmm.sort_values(by="bank", ascending=False)
+
+
+#----VZ----
+
+wma_results_vz = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_wma_profit(y, x, vz)
+        wma_results_vz = wma_results_vz.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+wma_results_vz = wma_results_vz.sort_values(by="bank", ascending=False)
+
+#The following calculates the results of each CAT, MMM, and VZ for each of the 
+# short-term and long-term value pairs using the exponential moving average crossover
+# strategy. The results are saved to a data frame.
+
+#----CAT----
+ema_results_cat = pd.DataFrame(columns=["short-period", "long-period", "bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_ema_profit(y, x, cat)
+        ema_results_cat = ema_results_cat.append({"short-period":x,"long-period":y,"bank":bank}, ignore_index=True)
+    
+ema_results_cat = ema_results_cat.sort_values(by="bank", ascending=False)
+
+#----MMM----
+ema_results_mmm = pd.DataFrame(columns=["Short-period", "Long-period", "Bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_ema_profit(y, x, mmm)
+        ema_results_mmm = ema_results_mmm.append({"Short-period":x,"Long-period":y,"Bank":bank}, ignore_index=True)
+    
+ema_results_mmm = ema_results_mmm.sort_values(by="Bank", ascending=False)
+
+
+#----VZ----
+
+ema_results_vz = pd.DataFrame(columns=["Short-period", "Long-period", "Bank"])
+
+for x in range(10,201,5):
+    for y in range(x+5,201,5):
+        bank, result = calculate_ema_profit(y, x, vz)
+        ema_results_vz = ema_results_vz.append({"Short-period":x,"Long-period":y,"Bank":bank}, ignore_index=True)
+    
+ema_results_vz = ema_results_vz.sort_values(by="Bank", ascending=False)
+
+#Relative strength
 
 cat['Change'] = cat['Price'].rolling(2).apply(lambda x : x[1]-x[0])
 cat['rsi30'] = cat['Change'].rolling(30).apply(calculate_rsi)
@@ -145,7 +216,10 @@ plt.ylim(bottom=20, top=80)
 plt.show()
 
 
+#The following calculates the results of each CAT, MMM, and VZ  
+# using the relative strength index# strategy. 
 
+bank, result = calculate_rsi_profit_50(cat, 30)
 
 
 
